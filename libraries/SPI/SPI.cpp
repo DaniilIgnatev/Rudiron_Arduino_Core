d3f7402af3602a06abd7d3ddc884e0c66e1f333b
Daniil <i.daniil2010@yandex.ru>
2022-07-05 13:09:28 +0400

    spi default begin

--- libraries/SPI/SPI.cpp
+++ libraries/SPI/SPI.cpp
@@ -62,7 +62,10 @@
    // initialized++; // reference count
    // SREG = sreg;
 
-   spi_instance.begin();
+    uint16_t SPH = lastSettings.dataMode % 2 == 0 ? SSP_SPH_1Edge : SSP_SPH_2Edge;
+    uint16_t SPO = lastSettings.dataMode < 2 ? SSP_SPO_Low : SSP_SPO_High;
+
+    spi_instance.begin(lastSettings.clock, SPH, SPO, SSP_WordLength8b, SSP_FRF_SPI_Motorola, SSP_HardwareFlowControl_SSE);
  }
 
  void SPIClass::end()
