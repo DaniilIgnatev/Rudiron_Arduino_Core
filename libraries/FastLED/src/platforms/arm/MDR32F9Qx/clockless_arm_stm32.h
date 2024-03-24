3a24b0ae562bb5c0dc4454bcf72a5b64c0fb51d9
Daniil Ignatev <folkestone1726@gmail.com>
2024-03-24 16:09:12 +0100

    fastled ws2812 fix for 64 and 80MHz

--- libraries/FastLED/src/platforms/arm/MDR32F9Qx/clockless_arm_stm32.h
+++ libraries/FastLED/src/platforms/arm/MDR32F9Qx/clockless_arm_stm32.h
@@ -74,6 +74,7 @@ protected:
     uint32_t portdata_zero;
     uint32_t portdata_one;
 
+#if(F_CPU == 64000000)
     inline void transmit_zero()
     {
         volatile char b;
@@ -168,6 +169,104 @@ protected:
         // b = 35;
         // b = 36;
     }
+#else
+    inline void transmit_zero()
+    {
+        volatile char b;
+
+        port->RXTX = portdata_one;
+        b = 0;
+        b = 1;
+        b = 2;
+        b = 3;
+        b = 4;
+        b = 5;
+        b = 6;
+       b = 7;
+       b = 8;
+       b = 9;
+    //    b = 10;
+    //    b = 11;
+
+        port->RXTX = portdata_zero;
+
+        b = 10;
+        b = 11;
+        b = 12;
+        b = 13;
+        b = 14;
+        b = 15;
+        b = 16;
+        b = 17;
+
+        b = 18;
+        b = 19;
+        b = 20;
+        b = 21;
+        b = 22;
+       b = 23;
+       b = 24;
+//        b = 25;
+
+        // b = 26;
+        // b = 27;
+        // b = 28;
+        // b = 29;
+        // b = 30;
+        // b = 31;
+        // b = 32;
+        // b = 33;
+    }
+
+    inline void transmit_one()
+    {
+        volatile char b;
+
+        port->RXTX = portdata_one;
+        b = 0;
+        b = 1;
+        b = 2;
+        b = 3;
+        b = 4;
+        b = 5;
+        b = 6;
+        b = 7;
+        b = 8;
+        b = 9;
+
+        b = 10;
+        b = 11;
+       b = 12;
+       b = 13;
+//        b = 14;
+//        b = 15;
+//        b = 16;
+//        b = 17;
+//        b = 18;
+
+        port->RXTX = portdata_zero;
+
+        b = 19;
+        b = 20;
+        b = 21;
+        b = 22;
+        b = 23;
+        b = 24;
+        b = 25;
+       b = 26;
+       b = 27;
+       b = 28;
+//
+//        b = 29;
+//        b = 30;
+//        b = 31;
+//        b = 32;
+//        b = 33;
+        // b = 34;
+        // b = 35;
+        // b = 36;
+    }
+#endif
 
     inline void transmit_byte(uint8_t data)
     {
